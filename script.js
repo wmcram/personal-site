cf7a5744cff20a44dbd2929dd3b7239f50934721
Wil Cram <wcram@wisc.edu>
2025-06-14 15:07:27 -0500

    feat: Index page, still no image

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-quotes = [
-    `"One should never try to prove anything that is not almost obvious." -Alexander Grothendieck`,
-    `"In mathematics you don't understand things. You just get used to them." -John von Neumann`,
-    `"Mathematics is the art of giving the same name to different things." -Henri Poincaré`,
+const quotes = [
+    `One should never try to prove anything that is not almost obvious. -Alexander Grothendieck`,
+    `In mathematics you don't understand things. You just get used to them. -John von Neumann`,
+    `Mathematics is the art of giving the same name to different things. -Henri Poincaré`,
+    `I am in a charming state of confusion. -Ada Lovelace`,
+    `If a machine is expected to be infallible, it cannot also be intelligent. -Alan Turing`,
+    `It is not knowledge, but the act of learning, not the possession of but the act of getting there, which grants the greatest enjoyment -Carl Friedrich Gauss`,
+    `Beware of bugs in the above code; I have only proved it correct, not tried it. -Donald Knuth`,
+    `Computer Science is no more about computers than astronomy is about telescopes. -Edsger Dijkstra`,
+    `One can imagine that the ultimate mathematician is one who can see analogies between analogies. -Stefan Banach`,
 ]
 
-document.querySelector("#quote").innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+// split into quote and author
+const index = Math.floor(Math.random() * quotes.length);
+document.querySelector("#quote").innerHTML = quotes[index].split('-')[0];
+document.querySelector("#author").innerHTML = '-' + quotes[index].split('-')[1];
